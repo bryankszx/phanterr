c0c81181418b7e4b0284ef696fc652ec3c9f998c
Bryan <bryanmzop@gmail.com>
2025-03-16 22:15:11 -0300

    Tela Login/Registro Feita

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('registerForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Cadastro realizado com sucesso!');
+});
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Login realizado com sucesso!');
+});
